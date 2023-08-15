11064a1a2433e275c3a09cc9c68ede23af3a80e8
TheHeelShow <tedimanosopi2002@gmail.com>
2023-08-15 18:46:58 +0200

    part2

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const likeCountSpan = document.getElementById("likeCount");
-        const likeButton = document.getElementById("likeButton");
-        let likes = 3; 
-        
-        likeButton.addEventListener("click", function() {
-            likes++;
-            likeCountSpan.textContent = likes;
-        });
+document.addEventListener("DOMContentLoaded", function() {
+    const likeButtonNeil = document.getElementById("likeButtonNeil");
+    const likeCountNeil = document.getElementById("likeCountNeil");
+    
+    const likeButtonNichole = document.getElementById("likeButtonNichole");
+    const likeCountNichole = document.getElementById("likeCountNichole");
+    
+    const likeButtonJim = document.getElementById("likeButtonJim");
+    const likeCountJim = document.getElementById("likeCountJim");
+
+    likeButtonNeil.addEventListener("click", function() {
+        likeCountNeil.textContent = parseInt(likeCountNeil.textContent) + 1;
+    });
+    
+    likeButtonNichole.addEventListener("click", function() {
+        likeCountNichole.textContent = parseInt(likeCountNichole.textContent) + 1;
+    });
+    
+    likeButtonJim.addEventListener("click", function() {
+        likeCountJim.textContent = parseInt(likeCountJim.textContent) + 1;
+    });
+});
